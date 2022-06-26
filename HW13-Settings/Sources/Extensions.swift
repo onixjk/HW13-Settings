85238b8aad8290771206285d16b15d68eab3bdea
onixjk <onixjkk@gmail.com>
2022-06-26 09:33:21 +0300

    Setup TableView.
    Add Metric. Add extensions. Setup TableView.

---
+++ HW13-Settings/Sources/Extensions.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension SettingsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Нажата ячейка \(indexPath.row)")
+    }
+}
